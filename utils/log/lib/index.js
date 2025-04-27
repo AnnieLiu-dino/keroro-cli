2c34b0790c3c7b280d1aae43c017869c33eba2a6
AnneLiu <anne.liu0825@gmail.com>
2025-04-27 09:40:02 +0800

    A: register command

--- utils/log/lib/index.js
+++ utils/log/lib/index.js
@@ -8,8 +8,9 @@ npmlog.headingStyle = {
   bg: "blue",
 };
 
-// 判断debug模式
+// 判断环境变量 是否debug模式
 npmlog.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "info";
+
 // 自定义log命令：log.success('xxx')
 npmlog.addLevel("success", 2000, { fg: "green", bold: true });
 
---
+++ utils/npm-info/lib/index.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const axios = require("axios");
+const urlJoin = require("url-join");
+const semver = require("semver");
+const log = require("@keroro-cli/log");
+
+async function getNpmInfo(npmName, registry) {
+  if (!npmName) return null;
+  const domain = registry || getDefaultRegistry();
+  const url = urlJoin(domain, npmName);
+  try {
+    const res = await axios.get(url);
+    const { status, data } = res;
+    if (status === 200) {
+      return data;
+    }
+  } catch (e) {
+    log.error("获取npm信息发生错误", e.message);
+    return null;
+  }
+}
+
+function getDefaultRegistry(isOrigin = true) {
+  const originDomain = "https://registry.npmjs.org";
+  const tbDomain = "https://registry.npm.taobao.org";
+  return isOrigin ? originDomain : tbDomain;
+}
+
+async function getVersions(name, registry) {
+  const { versions = {} } = await getNpmInfo(name, registry);
+  const list = Object.keys(versions).sort((prev, next) =>
+    semver.gt(next, prev)
+  );
+  return list;
+}
+
+async function getNpmLatestVersion(name, version, registry) {
+  const versions = await getVersions(name, registry);
+  const satisfyVersions = getSemverVersions(version, versions);
+  if (satisfyVersions && satisfyVersions.length > 0) {
+    return satisfyVersions[0];
+  }
+  return "1.0.0";
+}
+
+// 返回最新版本的具体版本号
+async function getNpmLatestVersionNum(name, registry) {
+  const versions = await getVersions(name, registry);
+  return versions[0] || "1.0.0";
+}
+
+function getSemverVersions(baseVersion, versions) {
+  versions = versions.filter((v) => semver.satisfies(v, `^${baseVersion}`));
+  return versions;
+}
+
+async function isLatestVersion(name, version, registry) {
+  const latestVersion = await getNpmLatestVersion(name, version, registry);
+  if (version && semver.gt(latestVersion, version)) {
+    return false;
+  }
+  return true;
+}
+
+module.exports = {
+  getNpmInfo,
+  getVersions,
+  getNpmLatestVersion,
+  getNpmLatestVersionNum,
+  isLatestVersion,
+  getDefaultRegistry,
+};
